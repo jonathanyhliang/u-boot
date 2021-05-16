8621d920c7b4434edf9b565a58dd92009a05f0c3
jonathanyhliang <jonathan.yh.liang@gmail.com>
2021-05-16 12:47:13 +0100

    correctly printed mram-cfg

--- drivers/can/test_can_driver.c
+++ drivers/can/test_can_driver.c
@@ -13,27 +13,30 @@ struct can_plat {
 
 static int can_of_to_plat(struct udevice *dev)
 {
-#if !CONFIG_IS_ENABLED(OF_PLATDATA)
     struct can_plat *plat = dev_get_plat(dev);
     const void *blob = gd->fdt_blob;
     int node = dev_of_offset(dev);
     if(fdtdec_get_int_array(blob, node, "bosch,mram-cfg", plat->mram_config_vals, MRAM_CFG_LEN)) {
-        return -EINVAL;          
+        return -EINVAL;
     }
-#endif
 
     return 0;
-};
+}
 
 static int can_probe(struct udevice *dev)
 {
     struct can_plat *plat = dev_get_plat(dev);
+    printf("mram-cfg: %d %d %d %d %d %d %d %d\n",
+            plat->mram_config_vals[0],
+            plat->mram_config_vals[1],
+            plat->mram_config_vals[2],
+            plat->mram_config_vals[3],
+            plat->mram_config_vals[4],
+            plat->mram_config_vals[5],
+            plat->mram_config_vals[6],
+            plat->mram_config_vals[7]);
 
-#if CONFIG_IS_ENABLED(OF_PLATDATA)
-    struct dtd_can *dtplat = &plat->dtplat;
-    memcpy((u32)plat->mram_config_vals, dtplat->bosch_mram_cfg, MRAM_CFG_LEN);
-#endif
-    printf("can probe\n");
+    return 0;
 }
 
 static const struct udevice_id can_of_match[] = {
